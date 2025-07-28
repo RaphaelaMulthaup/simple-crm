7b5f44471d289b6f530cc19dcbe3e7199bf07ea8
Raphaela Multhaup <raphaelamulthaup@icloud.com>
2025-07-28 22:27:39 +0200

    feat: add user button

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
-export class UserComponent {
-
-}
+export class UserComponent {}
