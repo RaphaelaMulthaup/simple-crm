4548cf2b70f322522f53f870ed6921a607895481
Raphaela Multhaup <raphaelamulthaup@icloud.com>
2025-07-30 20:03:00 +0200

    feat: input fields dialog add user

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-import { MatDialogContent, MatDialogModule } from "@angular/material/dialog";
-import {MatButtonModule} from '@angular/material/button';
+import { MatDialogContent, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-dialog-add-user',
-  imports: [MatDialogContent, MatDialogModule, MatButtonModule],
+  imports: [
+    MatDialogContent,
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+  ],
+  providers: [
+    provideNativeDateAdapter(), // ✅ moderner Weg
+  ],
   templateUrl: './dialog-add-user.component.html',
-  styleUrl: './dialog-add-user.component.scss'
+  styleUrl: './dialog-add-user.component.scss',
 })
-export class DialogAddUserComponent {
-
-}
+export class DialogAddUserComponent {}
