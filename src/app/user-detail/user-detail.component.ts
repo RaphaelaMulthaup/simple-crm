1d8eabdef4259b1349bb2224c8afc53170ee3d35
Raphaela Multhaup <raphaelamulthaup@icloud.com>
2025-08-05 09:09:43 +0200

    feat: read id from url

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from "@angular/material/card";
+import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user-detail',
   imports: [MatCardModule],
   templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.scss'
+  styleUrl: './user-detail.component.scss',
 })
 export class UserDetailComponent {
-
+  constructor(private route: ActivatedRoute) {}
+  private sub: Subscription = new Subscription();
+  userId: string | null = null;
+  ngOnInit(): void {
+    this.sub = this.route.paramMap.subscribe((params) => {
+      this.userId = params.get('id');
+      console.log('Neue ID:', this.userId);
+    });
+  }
 }
